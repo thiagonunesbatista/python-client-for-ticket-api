7cf6c53b8bff9a234b640a6abe6dac99cd140231
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2024-07-02 16:20:45 -0300

    feat: update menu helper messages

--- helpers/menu.py
+++ helpers/menu.py
@@ -1,11 +1,10 @@
 def goBackMenuMessage():
-    print("Retornando ao menu anterior...")
-    print("\n")
+    print("Retornando ao menu anterior...\n")
 
 
 def showWrongMenuMessage():
-    print("Esta opção não existe, tente novamente !!!")
+    print("Esta opção não existe, tente novamente !!!\n")
 
 
 def exitApp():
-    print("Saindo do APP...")
+    print("Saindo do APP...\n")
