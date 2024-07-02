31303c31af1d3cc7a4c9e0fd40aa2c7388bfa89e
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2024-07-02 12:35:13 -0300

    feat: add menu helpers

---
+++ helpers/menu.py
@@ -0,0 +1,10 @@
+def goBackMenuMessage():
+    print("Retornando ao menu anterior...")
+
+
+def showWrongMenuMessage():
+    print("Esta opção não existe, tente novamente !!!")
+
+
+def exitApp():
+    print("Saindo do APP...")
